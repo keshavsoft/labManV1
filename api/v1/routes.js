fee6cb3bd5a616b8b04a2b5ea526d408a0bd034a
keshavsoft <nknnkumar@yahoo.com>
2026-07-15 10:16:49 +0530

    bills crud

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { router as routerFromdoctors } from "./doctors/end-points.js";
 import { router as routerFromtests } from "./tests/end-points.js";
+import { router as routerFrombills } from "./bills/end-points.js";
 
-const router = express.Router();
+const router = express.Router()
+router.use("/bills", routerFrombills);;
 
 router.use("/tests", routerFromtests);
 router.use("/doctors", routerFromdoctors);;
