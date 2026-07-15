ee83b4f33bec863145e79f86089ff84de0d4ca5e
keshavsoft <nknnkumar@yahoo.com>
2026-07-15 10:10:44 +0530

    masters

--- Public/v5/bills/Index/FormLoad/DomContentLoaded/AddListeners/showAllHtmlId/BuildTable/getKSTableConfig.js
+++
@@ -1,5 +0,0 @@
-import configJson from "./configs/billShow.json" with { type: "json" };
-
-export const getKSTableConfig = async () => {
-    return structuredClone(configJson);
-};
--- Public/v5/bills/Index/FormLoad/DomContentLoaded/AddListeners/start.js
+++ Public/v5/bills/Index/FormLoad/DomContentLoaded/AddListeners/start.js
@@ -1,8 +1,6 @@
-import showAllHtmlId from "./showAllHtmlId/start.js";
 import crudHtmlId from "./crudHtmlId/start.js";
 
 const hookAllListeners = () => {
-    showAllHtmlId();
     crudHtmlId();
 };
 
